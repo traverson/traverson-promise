a4853453ba72e09e189e3ee907bef8d5bd6198f3
xnie <xin@slimpay.com>
2016-09-06 09:59:44 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# traverson-promise
+
+A deadly simple promise wrapper of [traverson](https://github.com/basti1302/traverson) based on bluebird!
--- __tests__/promise-test.js
+++ __tests__/promise-test.js
@@ -9,6 +9,10 @@ describe('traverson-promise: ', () => {
     api = traversonPromise.from(rootUri).json()
   })
 
+  /**
+   * According to Promises/A+, object that contains a then function will be considered a promise
+   * Theses tests are simple and naive, should test more later on
+   */
   it('get should return a promise object after called', () => {
     let ret = api.newRequest().get()
     expect(typeof ret.then).toBe('function')
